f65234b77834df53e44377aa314f39a596a437f9
Max Lavicka <max.lavicka@gmail.com>
2017-11-10 15:16:37 -0800

    Map info window integrated, search integrated with map component as well

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -28,11 +28,6 @@ export class MapContainer extends React.Component {
         }
     }
 
-    componentDidMount() {
-        console.log("FUCK");
-        console.log("MAP PROPS", this.props);
-    }
-
     onMarkerClick(props, marker, e) {
         this.setState({
             selectedPlace: props,
@@ -50,15 +45,11 @@ export class MapContainer extends React.Component {
             })
         }
     }
-    
-    reRender(lat, lng) {
-        this.setState({lat: lat, lng: lng});
-    }
 
     render () {
+        const context = this;
         return (
             <div style={mapWrapper}>
-            <button type="button" onClick={() => {this.props.mapCoords((Math.random() * 3 + 32),(Math.random() * 3 - 120), this.reRender.bind(this))} }>Click to Add Marker</button>
             <Map google={this.props.google} onClick={this.onMapClicked.bind(this)} style={style} initialCenter={{
                 lat: 34.0522,
                 lng: -118.2437
@@ -66,7 +57,6 @@ export class MapContainer extends React.Component {
                 {this.props.items.map((item, index) => 
                     <Marker onClick={this.onMarkerClick.bind(this)} position={ { lat: item.owner_lat, lng:item.owner_lng } } item={ item } key={index} />
                 )}
-        
                 <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} >
                     <div>
                         {(this.state.selectedPlace.item === null) ? null :
