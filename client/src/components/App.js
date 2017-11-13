43028e6d5da316f857a087d1070587614c6532de
Yi Yang Tan <yiytan@usc.edu>
2017-11-13 10:21:47 -0800

    Rebase done, email added, fb info is a global prop

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -49,21 +49,28 @@ class App extends React.Component {
 
                 {(this.props.isAuth ? (
                 <div>
-                    <Router>
-                        <div>
-                            <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-                                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <span className="navbar-brand">Good Traders</span>
-                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul className="navbar-nav mr-auto">
-                                        <li className="nav-item active"><Link to ='/'>Home</Link></li>
-                                        <li className='nav-item'><Link to='/add'>Add Item</Link></li>
-                                    </ul>
-                                </div>
-                            </nav>
-                            <Route exact path="/" render={(props) => ( <Home {...props} items={this.props.items} getItems={this.props.getItems} email={this.props.email}/>)} />
+                    <div>Hello... {context.props.name}</div>
+                    <img src={context.props.photo} width="10%" height="10%"/>
+                    <div>{context.props.email}</div> 
+
+
+<Router>
+    <div>
+<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <span className="navbar-brand">Good Traders</span>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active"><Link to ='/'>Home</Link></li>
+        <li className='nav-item'><Link to='/add'>Add Item</Link></li>
+        <li className='nav-item'><Link to='/list'>All Items </Link></li>
+    </ul>
+  </div>
+  </nav>
+  <Route exact path="/" render={(props) => ( <Home {...props} items={this.props.items} getItems={this.props.getItems}/>)} />
                             <Route exact path="/add" component={AddItem} />
                         </div>
                     </Router>
