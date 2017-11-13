7ed5dfccf9914b87b28bd5e120c73e0f78231c94
Niv Salamon <mnsalamon@gmail.com>
2017-11-13 15:41:55 -0800

    rebase merge

--- client/src/components/ItemEntry.js
+++ client/src/components/ItemEntry.js
@@ -6,30 +6,7 @@ const FontAwesome = require('react-fontawesome');
 const ItemEntry = (props) => {
 
     return (
-<<<<<<< HEAD
         <div className='item-wrapper'>               
-=======
-        <div className='item-wrapper'>
-            {console.log(props.clicked)}
-            {(props.clicked ? (
-                <div>
-                    <h2 className='item-header' onClick={ () => {props.select(props.item)} }>{props.item.item_name}</h2>
-                    <div className='row items-wrapper'>
-                        <div className='col-lg-4'>
-                            <img src={props.item.image_url} className='item-img'/>
-                        </div>
-                        <div className='col-lg-8'>
-                            <p className='item-location'> {props.item.location}</p>
-                            <p className='item-description'>{props.item.description}</p>
-                            <p><i className="fa fa-usd" aria-hidden="true"></i>{props.item.price}</p>
-                            <p className='item-type-wrapper'><span className='item-type'>Type:</span> {props.item.type}</p>
-                            <p className='email-seller-header'>{props.item.owner_email}</p>
-                            <Email email={props.email} item={props.item} />
-                        </div>
-                    </div>
-                </div>
-                ) :                 
->>>>>>> item-entry responsive
                 <div>
                     <h2 className='item-header' onClick={ () => {props.select(props.item)} }>{props.item.item_name}</h2>
                     <div className='row items-wrapper'>
